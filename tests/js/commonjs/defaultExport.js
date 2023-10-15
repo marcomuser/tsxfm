963891ee0adbce37c8b8fb53201988c8cac6a2f8
Marco Muser <marco.muser@enpal.de>
2023-10-15 18:57:11 +0200

    Add basic tests for js default nextLoad

---
+++ tests/js/commonjs/defaultExport.js
@@ -0,0 +1,8 @@
+Object.defineProperty(exports, "__esModule", { value: true });
+
+function helloWorld() {
+  return "hello world!";
+}
+
+module.exports = helloWorld;
+module.exports.default = helloWorld;
