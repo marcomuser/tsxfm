28ca0712ab5eb07fab8a127ac216a617b1537d58
Marco Muser <marco.muser@enpal.de>
2023-10-15 12:20:12 +0200

    Add return type annotations in all test fns

--- tests/scripting/searchParams.ts
+++ tests/scripting/searchParams.ts
@@ -1,4 +1,4 @@
-export function searchParams() {
+export function searchParams(): string | null {
   const url = new URL(import.meta.url);
   const searchParams = url.searchParams.get("hello");
   return searchParams;
