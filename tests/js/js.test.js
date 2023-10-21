fd362a5eb341c8f4734f265fb6de8640ff4ab5ea
Marco Muser <64737396+marcomuser@users.noreply.github.com>
2023-10-21 17:02:56 +0200

    Restructure test fixtures (#21)

--- tests/js/js.test.js
+++ tests/js/js.test.js
@@ -5,13 +5,15 @@ await import("#tsxfm");
 
 describe("js default nextLoad", async () => {
   it("handles hello world javascript function from ES module", async () => {
-    const { helloWorld } = await import("./helloWorld.js");
+    const { helloWorld } = await import("./fixtures/helloWorld.js");
 
     assert.equal(helloWorld(), "hello world!");
   });
 
   it("handles CommonJS module with simulated default export", async () => {
-    const { default: helloWorld } = await import("./commonjs/defaultExport.js");
+    const { default: helloWorld } = await import(
+      "./fixtures/commonjs/defaultExport.js"
+    );
 
     assert.equal(helloWorld(), "hello world!");
   });
--- tests/ts/ts.test.js
+++ tests/ts/ts.test.js
@@ -5,56 +5,62 @@ await import("#tsxfm");
 
 describe("ts transform", async () => {
   it("transforms hello world typescript function", async () => {
-    const { helloWorld } = await import("./helloWorld.js");
+    const { helloWorld } = await import("./fixtures/helloWorld.js");
 
     assert.equal(helloWorld(), "hello world!");
   });
 
   it("resolves import with .js file extension", async () => {
-    const { jsImport } = await import("./jsImport.js");
+    const { jsImport } = await import("./fixtures/jsImport.js");
 
     assert.equal(jsImport(), "hello world!");
   });
 
   it("resolves import with .ts file extension", async () => {
-    const { tsImport } = await import("./tsImport.js");
+    const { tsImport } = await import("./fixtures/tsImport.js");
 
     assert.equal(tsImport(), "hello world!");
   });
 
   it("resolves deep import", async () => {
-    const { deepImport } = await import("./deepImport.js");
+    const { deepImport } = await import("./fixtures/deepImport.js");
 
     assert.equal(deepImport(), "hello world!");
   });
 
   it("handles bare module specifier", async () => {
-    const { prettifiedCode } = await import("./prettifiedCode.js");
+    const { prettifiedCode } = await import("./fixtures/prettifiedCode.js");
 
     assert.equal(await prettifiedCode(), `console.log("hello world!");\n`);
   });
 
   it("transforms module with .mts file extension", async () => {
-    const { mts } = await import("./mts/mts.mjs");
+    const { mts } = await import("./fixtures/mts/mts.mjs");
 
-    assert(mts(), "hello world!");
+    assert.equal(mts(), "hello world!");
   });
 
   it("transforms subpath import with .js file extension", async () => {
-    const { subpathWithJsExt } = await import("./subpath/subpathWithJsExt.js");
+    const { subpathWithJsExt } = await import(
+      "./fixtures/subpath/subpathWithJsExt.js"
+    );
 
     assert.equal(subpathWithJsExt(), "hello world!");
   });
 
   it("transforms subpath import with .ts file extension", async () => {
-    const { subpathWithTsExt } = await import("./subpath/subpathWithTsExt.js");
+    const { subpathWithTsExt } = await import(
+      "./fixtures/subpath/subpathWithTsExt.js"
+    );
 
     assert.equal(subpathWithTsExt(), "hello world!");
   });
 
   it("transforms imports with search params", async () => {
-    // @ts-expect-error TypeScript 5.2 does not support this even though Node.js does
-    const { searchParams } = await import("./searchParams.js?hello=world");
+    const { searchParams } = await import(
+      // @ts-expect-error TypeScript 5.2 does not support this even though Node.js does
+      "./fixtures/searchParams.js?hello=world"
+    );
 
     assert.equal(searchParams(), "world");
   });
